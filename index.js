04cf717305e89cda956fc8c15a813accefce820a
dino <dino770310@gmail.com>
2020-10-26 16:05:15 +0800

    test

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
-var linebot = require('linebot');
-var express = require('express');
+'use strict';
 
-var bot = linebot({
-    channelId: '1654690588',
-    channelSecret: 'c3fb1a023f2f0e297ea3c8fceb3d5cd3',
-    channelAccessToken: '4SvaWSSOxYOYa9FyYtF6pTc3uOiBY0QbGSnCLEu6kQ28AJ5287n8groLeOga1GIbDOgJ4sN/Z+zboVcliVEohxMHFhXJUUeRDBdnBDKUgBZswuPIv9Q+8jhj3TBsVHl6iSPtV6YMHcEXF0Ad2xcyXgdB04t89/1O/w1cDnyilFU='
-});
+const line = require('@line/bot-sdk');
+const express = require('express');
+const getJSON = require('get-json');
+const moment = require('moment');
+const { GET_TIDAL_BY_DATE } = require('./api');
+// const { channelAccessToken, channelSecret, port } = require('./config');
+const tidalHandler = require('./handler/tidalHandler');
+const dbHandler = require('./server/dbHandler');
 
-bot.on('message', function(event) {
-    console.log(event); //把收到訊息的 event 印出來看看
-});
+const lineConfig = {
+        channelAccessToken: '4SvaWSSOxYOYa9FyYtF6pTc3uOiBY0QbGSnCLEu6kQ28AJ5287n8groLeOga1GIbDOgJ4sN/Z+zboVcliVEohxMHFhXJUUeRDBdnBDKUgBZswuPIv9Q+8jhj3TBsVHl6iSPtV6YMHcEXF0Ad2xcyXgdB04t89/1O/w1cDnyilFU=',
+        channelSecret: 'c3fb1a023f2f0e297ea3c8fceb3d5cd3'
+    }
+    // create LINE SDK client
+const client = new line.Client(lineConfig);
 
+// create Express app
+// about Express itself: https://expressjs.com/
 const app = express();
-const linebotParser = bot.parser();
-app.post('/', linebotParser);
 
-//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
-var server = app.listen(process.env.PORT || 8080, function() {
-    var port = server.address().port;
-    console.log("App now running on port", port);
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(lineConfig), (req, res) => {
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result))
+        .catch((err) => {
+            console.error(err);
+            res.status(500).end();
+        });
+});
+
+// event handler
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        // ignore non-text-message event
+        return Promise.resolve(null);
+    }
+    return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: '4545454'
+    });
+}
+
+// listen on port
+app.listen('3000', () => {
+    console.log(`cool now listening on 3000`);
 });
