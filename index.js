c60207f8a888649a4a8405766c4867e2824e068a
dino <dino770310@gmail.com>
2020-10-27 09:28:22 +0800

    10270928

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const line = require('@line/bot-sdk');
 const express = require('express');
 const getJSON = require('get-json');
 const moment = require('moment');
-// const { GET_TIDAL_BY_DATE } = require('./api');
-// const { channelAccessToken, channelSecret, port } = require('./config');
-// const tidalHandler = require('./handler/tidalHandler');
-const dbhandler = require('./server/dbhandler');
+const { GET_TIDAL_BY_DATE } = require('./api');
+const tidalhandler = require('./handler/tidalhandler');
+// const dbhandler = require('./server/dbhandler');
 
-console.log(typeof(dbhandler))
-const lineConfig = {
-        channelAccessToken: process.env.channelAccessToken,
-        channelSecret: process.env.channelSecret
-    }
-    // create LINE SDK client
-const client = new line.Client(lineConfig);
+// create LINE SDK client
+const client = new line.Client({
+    channelAccessToken: process.env.channelAccessToken,
+    channelSecret: process.env.channelSecret
+});
+
+let tidalData = [];
+getTidalData();
 
 // create Express app
 // about Express itself: https://expressjs.com/
@@ -39,9 +39,24 @@ function handleEvent(event) {
         // ignore non-text-message event
         return Promise.resolve(null);
     }
-    return client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: '4545454'
+
+    // 語句檢查
+    const userInputStr = event.message.text.replace(/\s+/g, ""); // 去空白
+    const tidalRegex = /^[^a-zA-Z0-9]{2,}潮汐/; // 開頭非英文或數字,接潮汐
+
+}
+
+// getTidalData
+function getTidalData() {
+    const fromDate = moment().format('YYYY-MM-DD');
+    const toDate = moment().add(3, 'days').format('YYYY-MM-DD');
+    getJSON(GET_TIDAL_BY_DATE(fromDate, toDate), function(error, response) {
+        let arr = [];
+        response.records.location.forEach(element => {
+            arr.push(element);
+        });
+        tidalData = arr;
+        console.log('getTidalData OK');
     });
 }
 
